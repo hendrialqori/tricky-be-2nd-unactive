03fc582bed958705a199740da02dfd066a6dd8bd
alqory <teamhendri18@gmail.com>
2022-07-24 10:02:26 +0700

    add procfile file

--- src/configs/index.js
+++ src/configs/index.js
@@ -5,7 +5,7 @@ import sequelize from 'sequelize'
 config()
 const DB = process.env
 // export const Blogs = new Sequelize(`postgres://${DB.DB_USER}:${DB.DB_PASSWORD}@${DB.DB_HOST}:${DB.DB_PORT}/${DB.DB_NAME}`)
-const Blogs = new sequelize(`${process.env.DATABASE_URI}`)
+const Blogs = new Sequelize(`${process.env.DATABASE_URI}`)
 
 export const dbAuthenticate = async () => {
     try {
