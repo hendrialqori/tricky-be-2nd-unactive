85f9607440c2715d72ba65757418c5d7f4117313
alqory <teamhendri18@gmail.com>
2022-07-24 10:16:48 +0700

    resolve error db config

--- src/configs/index.js
+++ src/configs/index.js
@@ -4,7 +4,7 @@ import { config } from 'dotenv'
 config()
 const DB = process.env
 // export const Blogs = new Sequelize(`postgres://${DB.DB_USER}:${DB.DB_PASSWORD}@${DB.DB_HOST}:${DB.DB_PORT}/${DB.DB_NAME}`)
-const Blogs = new Sequelize(`${process.env.DATABASE_URI}`)
+export const Blogs = new Sequelize(`${process.env.DATABASE_URI}`)
 
 export const dbAuthenticate = async () => {
     try {
